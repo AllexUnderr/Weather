bfd26ccd03bd6f7527186c66274586c7cace95f0
Alexander <painkillerqq@gmai.com>
2022-12-24 18:49:38 +0700

    geocoding and reverse geocoding

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -25,20 +25,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         binding.insertButton.setOnClickListener {
-            val latitude = binding.latitudeEditText.text.toString().toDoubleOrNull()
-            val longitude = binding.longitudeEditText.text.toString().toDoubleOrNull()
+            val nameObject = binding.objectNameEditText.text.toString()
 
-            if (latitude == null || longitude == null)
+            if (nameObject.isBlank())
                 return@setOnClickListener
 
-            if (latitude in -90.0..90.0 && longitude in -180.0..180.0) {
-                setTemperature(latitude, longitude)
-                map.clear()
-                val coordinates = LatLng(latitude, longitude)
-                map.addMarker(MarkerOptions().position(coordinates))
-                map.moveCamera(CameraUpdateFactory.newLatLng(coordinates))
-            } else {
-                binding.temperatureTextView.text = getString(R.string.coordinates_error)
+            Weather().geocoding(nameObject) { geocoding ->
+                if (geocoding.isEmpty()) {
+                    binding.objectNameTextView.text = getString(R.string.object_name_error)
+                    binding.applicationTitleTextView.text = getString(R.string.app_name)
+                } else {
+                    val (name, latitude, longitude) = geocoding.first()
+
+                    Weather().getWeather(latitude, longitude) {
+                        binding.objectNameTextView.text =
+                            getString(R.string.temperature, it.main.temp.toString())
+                    }
+                    binding.applicationTitleTextView.text = name
+
+                    setCoordinates(latitude, longitude)
+                    setMarker(LatLng(latitude, longitude))
+                }
             }
         }
     }
@@ -46,19 +53,36 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        map.setOnMapClickListener {
-            map.clear()
-            map.addMarker(MarkerOptions().position(it))
-            setTemperature(it.latitude, it.longitude)
-            binding.latitudeEditText.setText(it.latitude.toString())
-            binding.longitudeEditText.setText(it.longitude.toString())
+        map.setOnMapClickListener { marker ->
+            val latitude = marker.latitude
+            val longitude = marker.longitude
+
+            setCoordinates(latitude, longitude)
+            setMarker(marker)
+
+            Weather().getObjectName(latitude, longitude) {
+                if (it.isEmpty()) {
+                    binding.objectNameTextView.text = getString(R.string.object_coordinates_error)
+                    binding.applicationTitleTextView.text = getString(R.string.app_name)
+                } else {
+                    binding.applicationTitleTextView.text = it.first().name
+                    Weather().getWeather(latitude, longitude) { weatherData ->
+                        binding.objectNameTextView.text =
+                            getString(R.string.temperature, weatherData.main.temp.toString())
+                    }
+                }
+            }
         }
     }
 
-    private fun setTemperature(latitude: Double, longitude: Double) {
-        Weather().getWeather(latitude, longitude) {
-            binding.temperatureTextView.text =
-                getString(R.string.temperature, it.main.temp.toString())
-        }
+    private fun setCoordinates(latitude: Double, longitude: Double) {
+        binding.latitudeTextView.text = getString(R.string.latitude_template, latitude)
+        binding.longitudeTextView.text = getString(R.string.longitude_template, longitude)
+    }
+
+    private fun setMarker(latLng: LatLng) {
+        map.clear()
+        map.addMarker(MarkerOptions().position(latLng))
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng))
     }
 }
--- app/src/main/java/weatherAPI/WeatherAPI.kt
+++ app/src/main/java/weatherAPI/WeatherAPI.kt
@@ -5,11 +5,24 @@ import retrofit2.Call
 import retrofit2.http.Query
 
 interface WeatherAPI {
-    @GET("weather")
-    fun getWeather(
+    @GET("data/2.5/weather")
+    fun getWeatherByCoordinates(
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
         @Query("units") unit: String,
         @Query("appid") APIKey: String,
     ): Call<WeatherData>
+
+    @GET("geo/1.0/direct")
+    fun getWeatherByObject(
+        @Query("q") objectName: String,
+        @Query("appid") APIKey: String,
+    ): Call<List<Geocoding>>
+
+    @GET("geo/1.0/reverse")
+    fun getObjectName(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") APIKey: String,
+    ): Call<List<ObjectName>>
 }
